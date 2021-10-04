46cfecfc044c06655d7d10f823524a6cb406e09b
Bao Pham <baopq19@gmail.com>
2021-10-04 11:21:51 +0700

    first commit

---
+++ src/TailwindComponent/DemoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function DemoGrid() {
+    return (
+        <div className="container">
+            <div className="grid">
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+            </div>
+        </div>
+    )
+}
