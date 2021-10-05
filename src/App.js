9373ee443ec085b09262c1a48db4602028286bf4
Bao Pham <baopq19@gmail.com>
2021-10-05 12:18:17 +0700

    Redux config complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import DemoGrid from './TailwindComponent/DemoGrid';
 
 function App() {
   return (
     <div className="App">
-      <div className="md:container md:mx-auto">
-        <DemoGrid></DemoGrid>
-      </div>
+      
     </div>
   );
 }
