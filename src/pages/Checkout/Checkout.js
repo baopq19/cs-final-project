99f82447b3490d2174650fe6baa29da8b663d122
Bao Pham <baopq19@gmail.com>
2021-10-18 18:14:58 +0700

    booking under construction

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -3,8 +3,11 @@ import './Checkout.css';
 
 export default function Checkout(props) {
     return (
-        <div>
-            Checkout
+        <div className='pt-32 bg-gray-300'>
+            <h2 className='text-center'>Xin lỗi, Tính năng đang được phát triển</h2>
+            <div className='flex justify-center'>
+                <img src='https://software.ornl.gov/img/construction.svg' alt='under-construction'></img>
+            </div>
         </div>
     )
 }
